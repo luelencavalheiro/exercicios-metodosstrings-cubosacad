aef1cb49ea36bd9e99cb2bcca7299877fc97b8f3
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-08-27 21:26:19 -0300

    Resolução Exercicios

--- 02 - Sistema para Formatar Números/index.js
+++ 02 - Sistema para Formatar Números/index.js
@@ -1,2 +1,16 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678000199";
+
+if (cpf.length != 11) {
+    console.log("CPF inválido");
+} else {
+    const cpfFormatado = `${cpf.slice(0, 3)}.${cpf.slice(3, 6)}.${cpf.slice(6, 9)}-${cpf.slice(9)}`;
+    console.log(cpfFormatado);
+}
+
+if (cnpj.length != 14) {
+    console.log("CNPJ inválido");
+} else {
+    const cnpjFormatado = `${cnpj.slice(0, 2)}.${cnpj.slice(2, 5)}.${cnpj.slice(5, 8)}/${cnpj.slice(8, 12)}-${cnpj.slice(12)}`;
+    console.log(cnpjFormatado);
+}
