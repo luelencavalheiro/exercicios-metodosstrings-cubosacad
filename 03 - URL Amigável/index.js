072c264bc8c91eccbe62b63c5dcb1173d2af2c68
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-08-31 19:17:25 -0300

    Resolução Exercicios

--- 03 - URL Amigável/index.js
+++ 03 - URL Amigável/index.js
@@ -1,1 +1,11 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+const url = texto.toLowerCase().split(" ");
+let urlFormatada = '';
+
+for (let i = 0; i < url.length; i++) {
+
+    urlFormatada += `-${url.length[i]}`;
+
+}
+console.log(urlFormatada);
--- 04 - Sistema de Formatação de Dados/index.js
+++ 04 - Sistema de Formatação de Dados/index.js
@@ -1,3 +1,23 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+// verificando se o identificador tem 6 digitos
+if (identificador != 6) {
+
+    identificador = identificador.padStart(6, "0")
+}
+
+// formatando o nome
+let nomeFormatado = "";
+const arrayNome = nome.split(" ");
+for (let item of arrayNome) {
+    let primeiraLetra = item.slice(0, 1);
+    let restanteNome = item.slice(1);
+    nomeFormatado += primeiraLetra.toUpperCase() + restanteNome.toLowerCase() + " ";
+
+}
+nomeFormatado.trim();
+
+console.log(nomeFormatado);
+
